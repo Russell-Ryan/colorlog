5347d262b39a2b5b03553eabb5f77e3cc09cfb39
Russell Ryan <rryan@jamerson.local>
2020-08-09 22:19:20 -0400

    updated the testing for v2.0

--- test_colorlog.py
+++ test_colorlog.py
@@ -1,14 +1,10 @@
 from colorlog import ColorLog
 import sys
 
-def init():
-    log=ColorLog(stdout=False,stderr=False)
-
-
 def test():
 
     # get the name of the log (to make changes)
-    log=sys.stdout
+    log=ColorLog(root='test_colorlog')
 
     # show the standard levels
     print('[info]an informational message')
@@ -18,7 +14,7 @@ def test():
     print('basic printing')
 
     # add a personal level
-    log.addlevel('mine',foreground='magenta')
+    log.add_level('mine',foreground='magenta')
     print('[mine]personal message')
 
     # change the formatting of an existing level
@@ -30,25 +26,45 @@ def test():
     print('[info]another changed, informational message')
 
     # turn off the ascii logging
-    log.logfile.disable()
-    print('[warn]hide a message from the logfile')
-    log.logfile.enable()
+    log.logging=False
+    print('[warn]hide a message from the logfile, but show to terminal')
+    log.logging=True  # ok turn it back on
     
     # turn off the STDOUT
-    log.stdout.disable()
-    print('suppress STDout')
-    log.stdout.enable()
+    log.printing=False
+    print('suppressed STDout')
+    log.printing=True
     print("and... we're back")
 
-    # raise an error and see that the log is updated too!
-    raise RuntimeError('this is an exception')
+    # maybe you don't like the timestamps?  turn them off:
+    log.timestamp=False
+    print('[info]no timestamps now')
 
 
-    
+    # you can even close, and reopen the file
+    log.close()
+    print('nothing gets logged, since the file is closed')
+    log.reopen()
+    print("and we're back after closing")
+
+    # you can disable certain modes.
+    log.disable('debug') # turns off the debugging level
+    print("[debug]nothing happened, since this is off")
+    # well, there's an issue yet to be dealt with where it prints a blank line.
+
+    # it works with tqdm, sort-of
+    import tqdm
+    import inspect
+    import time
+    inspect.builtins.print=tqdm.tqdm.write   # need to do this :(
+
+    for i in tqdm.tqdm(range(10)):
+        time.sleep(0.1)
+        print('[info]{}'.format(i))
 
 
 if __name__=='__main__':
-    init()
+
     test()
     
 
